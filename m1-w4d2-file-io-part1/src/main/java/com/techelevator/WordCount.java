3e812c50eff1df3cba9349bd9af230ddea4e1161
Laura Stephens <laura@neutralspace.org>
2017-02-07 11:47:51 -0500

    Tuesday's pair exercise complete

--- m1-w4d2-file-io-part1/src/main/java/com/techelevator/WordCount.java
+++ m1-w4d2-file-io-part1/src/main/java/com/techelevator/WordCount.java
@@ -1,10 +1,62 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class WordCount {
 
-	public static void main(String[] args) {
-		
 
+	public static void main(String[] args) {
+		try(Scanner userInput = new Scanner(System.in)){
+			System.out.print("Enter file path >>> ");
+			String filePath = userInput.nextLine();
+			
+			try {
+				File searchFile = getSearchFile(filePath);
+				try(Scanner fileInput = new Scanner(searchFile)){ 
+					int sentNumber = 1;
+					int count = 0;
+					while(fileInput.hasNextLine()) {
+						String line = fileInput.nextLine();
+						
+						if(line.contains(".")||line.contains("?")||line.contains("!")){
+							sentNumber ++;
+						}
+						
+						String [] words = line.split(" ");
+						for(String word : words){
+							count ++;
+						}
+						
+					} System.out.println("Number of sentences: " + sentNumber);
+					System.out.println("Number of words: " + count);
+				
+		}
+				
+			} catch (FileNotFoundException ex) {
+				System.out.println(ex.getMessage());
+				System.exit(1);  
+			} catch (IOException ex) {
+				System.out.println(ex.getMessage());
+				System.exit(1);
+			}
+		}
 	}
+	
+	private static File getSearchFile(String filePath) throws IOException{
+		File searchFile = new File(filePath);
+		
+		if(!searchFile.exists()) {
+			throw new FileNotFoundException("Search file: " + filePath + " does not exist");
+		}
+		
+		if(!searchFile.isFile()) {
+			throw new IOException("Search file: " + filePath + " exists but is not a file");
+			}
+			
+			return searchFile;
+		}
 
 }
